768d67840ec71810fbafae5313901c2ec9f13a5f
Huseyin Kaya <hkayabilisim@gmail.com>
2023-09-25 21:57:23 +0300

    Engine is embedded in a portal v0.0.1

--- docs/index.rst
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c544036eaadf2037605d86ec18230028fa57eaefa0e3a6f18afc4bce94cee39c
-size 460
--- docs/make.bat
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bca6b3244f115b8f42cbc2bb866f1061b95eb6f01df1891e2ac68f03f7569d7b
-size 800
